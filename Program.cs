f433c214495c2e035c2f9e8ad765fa1e02d771ab
Redstoney96 <PineOS@Redstoney>
2026-02-24 16:10:26 +0100

    Repair code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 using Microsoft.AspNetCore.SignalR;
 
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<ChatHub>("/chat");
+
+app.MapGet("/", () => "The MSGBox Messager Server is online");
+
+app.Run();
+
+// Message Server main class
+// Take messages an send it to other clients
+// Create private rooms
 public class ChatHub : Hub
 {
     private static readonly Dictionary<string, string> UserKeys = new();
