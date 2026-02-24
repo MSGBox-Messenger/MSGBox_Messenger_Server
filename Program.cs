c7109baa49561db3093fa6cae004f212d02b2d25
Redstoney96 <PineOS@Redstoney>
2026-02-24 16:06:00 +0100

    Update the end to end encryption logic

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
 using Microsoft.AspNetCore.SignalR;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddSignalR();
-
-var app = builder.Build();
-
-app.MapHub<ChatHub>("/chat");
-
-app.MapGet("/", () => "The MSGBox Messager Server is online");
-
-app.Run();
-
-// Message Server main class
-// Take messages an send it to other clients
-// Create private rooms
 public class ChatHub : Hub
 {
-
     private static readonly Dictionary<string, string> UserKeys = new();
+
     public void RegisterKey(string user, string publicKeyBase64)
     {
         UserKeys[user] = publicKeyBase64;
@@ -27,7 +11,7 @@ public class ChatHub : Hub
 
     public string GetUserKey(string user)
     {
-        return UserKeys.TryGetValue(user, out var key) ? key : null;
+        return UserKeys.ContainsKey(user) ? UserKeys[user] : null;
     }
 
     public async Task SendToGroup(string chatName, string user, string message)
@@ -39,6 +23,7 @@ public class ChatHub : Hub
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, chatName);
     }
+
     public async Task LeaveChat(string chatName)
     {
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatName);
